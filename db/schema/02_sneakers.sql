1500d1ae4574e8d483223a35b181ab3f344cf0d8
Xiaowen Zheng <zxw880507@gmail.com>
2020-10-17 19:17:21 +0000

    Changing data type to text

--- db/schema/02_sneakers.sql
+++ db/schema/02_sneakers.sql
@@ -6,12 +6,12 @@ CREATE TABLE sneakers (
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   title VARCHAR(255) NOT NULL,
-  brand VARCHAR(255) NOT NULL,
+  brand TEXT NOT NULL,
   price INTEGER NOT NULL DEFAULT 0,
   size SMALLINT NOT NULL DEFAULT 0,
   model_year INTEGER NOT NULL DEFAULT 0,
-  thumbnail_photo_url VARCHAR(255) NOT NULL,
-  main_photo_url VARCHAR(255) NOT NULL,
+  thumbnail_photo_url TEXT NOT NULL,
+  main_photo_url TEXT NOT NULL,
 
   date_posted DATE NOT NULL,
   country VARCHAR(255) NOT NULL,
