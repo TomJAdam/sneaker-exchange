df742d2a475681700ef60a9b005f4fb98b663b4c
Xiaowen Zheng <zxw880507@gmail.com>
2020-10-18 23:14:56 +0000

    pagination

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,6 @@
 $(() => {
 
-    console.log('working');
+
 
     //append sneakers to list
     const appendSneakers = data => {
@@ -13,10 +13,12 @@ $(() => {
 
         const $sneaker = $(`
 <div class="w3-quarter">
-            <img src=${thumbnail_photo_url}>
-            <h3>${title}</h3>
-            <p>${price}</p>
-        </div>
+          <img src=${thumbnail_photo_url}>
+          <div class='info'>
+          <h3>${title}</h3>
+          <p>${price}</p>
+          </div>
+      </div>
 `);
         $listGrid.append($sneaker);
     };
@@ -32,7 +34,45 @@ $(() => {
             });
         });
 
+    // const insertPageLink = content => {
+    //     $(`<a href="#" class="w3-bar-item w3-button w3-hover-black">${content}</a>`).insertBefore($('#next'));
+    // };
+    const paginator = (current, last) => {
+        let range = [],
+            rangeWithDots = [],
+            delta = 1,
+            left = current - delta,
+            right = current + delta + 1,
+            l;
 
+        for (let i = 1; i <= last; i++) {
+            if (i === 1 || i === last || (i >= left && i < right)) {
+                range.push(i);
+            }
+        }
+        let length = range.length;
+        range.forEach(page => {
+            if (l) {
+                if (range.length < 5) {
+                    while (page - l >= 2 && length < 5) {
+                        rangeWithDots.push(++l);
+                        length++;
+                    }
+                } else if (page - l !== 1) {
+                    rangeWithDots.push('...');
+                }
+            }
+            rangeWithDots.push(page);
+            l = page;
+        });
+        return rangeWithDots;
+
+
+    };
+
+    for (let i = 1; i <= 20; i++) {
+        console.log(paginator(i, 20));
+    }
 
 
 
