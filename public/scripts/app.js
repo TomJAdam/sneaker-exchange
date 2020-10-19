18c9548f9fe32b241028712f42bcb6bf0d8cc0fb
Xiaowen Zheng <zxw880507@gmail.com>
2020-10-19 08:54:05 +0000

    ajax query for list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,41 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+
+    const listSneakers = (limit, currentPage = 1) => {
+        $.get({
+                url: `api/sneakers?page=${currentPage}`,
+            })
+            .then(res => {
+                const { count, data } = res;
+                const lastPage = Math.ceil(count / limit);
+                $('#list-grid').empty();
+                data.forEach(row => {
+                    appendSneakers(row);
+                });
+                $('#page-anchor').empty();
+                pagination(currentPage, lastPage);
+            });
+    };
+
+    // const sortSneakers = (formData, limit) => {
+    //     const data = formData.serialize();
+    //     $.post({
+    //             url: 'api/sneakers',
+    //             data
+    //         })
+    //         .then(res => {
+    //             const page = Math.ceil(res.length / limit);
+    //             res.forEach(data => {
+    //                 appendSneakers(data);
+    //             });
+    //         })
+    //         .catch(err => console.log(`Found Error: `, err));
+    // };
+
+
+    window.queries = {
+        listSneakers
+    };
+
+
+
 });
