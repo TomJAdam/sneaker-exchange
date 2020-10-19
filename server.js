352ea4198cae390edc508d0db25a1846d3bc62ff
Xiaowen Zheng <zxw880507@gmail.com>
2020-10-19 09:02:29 +0000

    remove comment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
-// app.set("view engine", "ejs"); ignore the ejs template ---- (This is a total crap)
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
     src: __dirname + "/styles",
