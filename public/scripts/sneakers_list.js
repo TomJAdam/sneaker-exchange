524ef00f264ba67d044e81971ddbc1e3201f40f8
Tom Adam <tom.adam21@gmail.com>
2020-10-19 18:05:22 +0000

    changes to main-grid design

--- public/scripts/sneakers_list.js
+++ public/scripts/sneakers_list.js
@@ -13,8 +13,12 @@ $(() => {
 <div class="w3-quarter">
       <img src=${thumbnail_photo_url}>
       <div class='info'>
-      <h3>${title}</h3>
-      <p>${price}</p>
+      <h4>${title}</h4>
+      <h3><b>$${price}</b></h3>
+      <footer>
+      <button class="w3-button w3-white w3-border w3-border-green w3-hover-green w3-round-large">buy now</button>
+      <i class="fab fa-gratipay"></i>
+      </footer>
       </div>
   </div>
 `);
