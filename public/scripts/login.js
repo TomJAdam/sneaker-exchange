06f74a9eca20e9944054da2aa1c72b241ce94b31
Tom Adam <tom.adam21@gmail.com>
2020-10-20 18:05:20 +0000

    login and register working with database and cookies

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,18 +1,13 @@
-// register
-
 $(() => {
 
-  $("#register-form").submit(function(e) {
+  $("#login-form").submit(function(e) {
     e.preventDefault();
     const data = $(this).serialize();
     console.log('data :', data);
     // $('.post-item form').trigger("reset");
     $.post({
-      url: "/user/register",
+      url: "/user/login",
       data: data
-    })
-    .then(() => {
-      res.redirect("/");
     })
   })
 
